4db246601e41cacb3b2eb2668162f9aafa845081
Rich Coe <rich.coe2@gmail.com>
2011-07-19 18:02:12 -0500

    decode values

--- phat.c
+++ phat.c
@@ -12,6 +12,7 @@
 #include "talloc.h"
 #include "rbtree.h"
 
+#define _FILE_OFFSET_BITS 64
 #define MAGIC_HEADER 0x4a415641   // JAVA
 
 struct jdump {          // java dump
@@ -26,20 +27,35 @@ struct jdump {          // java dump
     struct _cinfo *javaLangClass, *javaLangString, *javaLangClassLoader;
 };
 
-struct hobj {           // heap object
+struct _hobject {           // heap object
 #define H_INSTANCE 0x01
 #define H_VARRAY   0x02
 #define H_OARRAY   0x03
     int htype;
-    long long heapId;
+    long long instId, classId;
+    long fpos;
+    char resolved, visit;
+    unsigned int count;
+    int size;
+    union {
+        long long *ident;
+        unsigned char *b;
+        unsigned short *c;
+        unsigned int *i;
+        unsigned long long *j;
+        float *f;
+        double *d;
+    } val;
 };
+typedef struct _hobject hobject;
 
 struct _finfo {         // Field Info
     long long ident;    // field identifier
     char *name;         // [decoded] field name
     char ftype;         // field type
     char resolved;      // passed through resolver
-    int valsz;
+    int valsz;          // sizeof object
+    off_t offset;       // for resolved objects
     union {
         long long ident;
         unsigned char b;
@@ -61,7 +77,8 @@ struct _cinfo {         // Class Info
     int tfields;
     char resolved;
     finfo *statics;
-    finfo *fields;
+    finfo *fields;              // this class's fields, self
+    finfo **values;             // this class's value fields, self + super(s)
 };
 typedef struct _cinfo cinfo;
 
@@ -348,13 +365,16 @@ readIdent(struct jdump *jf)
     return (long long) lval;
 }
 
-struct hobj *
-makeObj(trbt_tree_t *tab, int ht, long long cid)
+hobject *
+makeObj(trbt_tree_t *tab, int ht, long long iid, long long cid)
 {
-    struct hobj *ho = (struct hobj *) talloc(tab, struct hobj);
+    hobject *ho = (hobject *) talloc(tab, hobject);
 
     ho->htype = ht;
-    ho->heapId = cid;
+    ho->classId = cid;
+    ho->instId = iid;
+    ho->fpos = 0;
+    ho->resolved = ho->visit = 0;
     return ho;
 }
 
@@ -532,7 +552,7 @@ readArray(struct jdump *jf, unsigned hsize, int prim)
     unsigned int stackId, isz;
     char primSig = 0x00;
     int elsz;
-    struct hobj *ho;
+    hobject *ho;
 
     ide = readIdent(jf);
     readUint(jf->fin, &stackId);
@@ -564,46 +584,47 @@ readArray(struct jdump *jf, unsigned hsize, int prim)
         }
     }
     if (0x00 != primSig) {
-        fseek(jf->fin, elsz * isz, SEEK_CUR);
         hsize -= elsz * isz;
-        // ho = makeObj(jf->hTable, H_VARRAY, elemClassId);
+        ho = makeObj(jf->hTable, H_VARRAY, ide, elemClassId);
+        ho->fpos = ftello(jf->fin);
+        ho->size = elsz;
+        ho->count = isz;
+        fseek(jf->fin, elsz * isz, SEEK_CUR);
     } else {
-        fseek(jf->fin, jf->identsz * isz, SEEK_CUR);
         hsize -= jf->identsz * isz;
-        // ho = makeObj(jf->hTable, H_OARRAY, elemClassId);
+        ho = makeObj(jf->hTable, H_OARRAY, ide, elemClassId);
+        ho->fpos = ftello(jf->fin);
+        ho->size = jf->identsz;
+        ho->count = isz;
+        fseek(jf->fin, jf->identsz * isz, SEEK_CUR);
     }
-    // trbt_insert32(jf->hTable, ide, ho);
+    trbt_insert32(jf->hTable, ide, ho);
     return hsize;
 }
 
 void
-prclass(trbt_node_t *node)
+printClass(trbt_node_t *node)
 {
     cinfo *ci;
     if (NULL == node)
         return;
-    prclass(node->left);
+    printClass(node->left);
     ci = (cinfo *) node->data;
     printf("0x%x instance 0x%x %d %s\n", ci->ident, ci->nident, ci->count, ci->name);
-    prclass(node->right);
+    printClass(node->right);
 }
 
-void
-printclass(trbt_tree_t *cTable)
+unsigned long
+hashKey(char *key)
 {
-    cinfo *ci;
-    prclass(cTable->root->left);
-    ci = (cinfo *) cTable->root->data;
-    printf("0x%x instance 0x%x %d %s\n", ci->ident, ci->nident, ci->count, ci->name);
-    prclass(cTable->root->right);
+    unsigned long ckey = crc32(0, Z_NULL, 0);
+    return crc32(ckey, key, strlen(key));
 }
 
 cinfo *
 findClass(struct jdump *jf, char *cname)
 {
-    unsigned long ckey = crc32(0, Z_NULL, 0);
-    ckey = crc32(ckey, cname, strlen(cname));
-    return (cinfo *) trbt_lookup32(jf->rcTable, ckey);
+    return (cinfo *) trbt_lookup32(jf->rcTable, hashKey(cname));
 }
 
 int
@@ -615,7 +636,7 @@ resolveSClassSize(struct jdump *jf, cinfo *ci)
         return;
 
     super = (cinfo *) trbt_lookup32(jf->cTable, ci->superId);
-    return ci->cfields + resolveSuperclass(jf, super);
+    return ci->cfields + resolveSClassSize(jf, super);
 }
 
 void
@@ -633,14 +654,28 @@ resolveField(struct jdump *jf, finfo *fi)
     fi->resolved = 1;
 }
 
+cinfo *
+getSuperClass(struct jdump *jf, cinfo *ci)
+{
+    return (cinfo *) trbt_lookup32(jf->cTable, ci->superId);
+}
+
+finfo *
+copyField(cinfo *parent, finfo *src)
+{
+    finfo *dest = (finfo *) talloc(parent, finfo);
+    memcpy(dest, src, sizeof(finfo));
+    return dest;
+}
+
 void
-resClassNode(struct jdump *jf, cinfo *ci)
+resolveClassNode(struct jdump *jf, cinfo *ci)
 {
     cinfo *super;
     int i;
     if (ci->resolved)
         return;
-    ci->tfields = ci->cfields + resolveSClassSize(jf, ci);
+    ci->tfields = resolveSClassSize(jf, ci);
 
     // loader
     // signers
@@ -649,31 +684,247 @@ resClassNode(struct jdump *jf, cinfo *ci)
     for (i = 0; i < ci->cstats; i++) 
         resolveField(jf, ci->statics + i);
 
-    if (0 != (super = (cinfo *) trbt_lookup32(jf->cTable, ci->superId)))
-        resClassNode(jf, super);
+    if (0 != (super = getSuperClass(jf, ci)))
+        resolveClassNode(jf, super);
+
+    if (0 < ci->tfields) {
+        int target, index, fieldNo;
+        cinfo *cc = ci;
+
+        ci->values = talloc_array(ci, finfo *, ci->tfields);
+        target = ci->tfields - cc->cfields;
+        fieldNo = index = 0;
+        for (i = 0; i < ci->tfields; i++, fieldNo++) {
+            finfo *field, **value;
+            while (fieldNo >= cc->cfields) {
+                cc = getSuperClass(jf, cc);
+                fieldNo = 0;
+                target -= cc->cfields;
+            }
+            value = ci->values + target + fieldNo;
+            if (cc == ci) {
+                field = *value = cc->fields + fieldNo;
+                field->name = (char *) trbt_lookup32(jf->sbTable, (long) field->ident);
+            } else
+                field = *value = copyField(ci, cc->fields + fieldNo);
+
+            field->offset = index;
+            switch (field->ftype) {
+            case 'L': 
+            case '[':  index += jf->identsz;  break;
+            case 'Z': 
+            case 'B':  index += 1; break;
+            case 'S': 
+            case 'C':  index += 2; break;
+            case 'F':
+            case 'I':  index += 4; break;
+            case 'D':
+            case 'J':  index += 8; break;
+            }
+        }
+    }
     ci->resolved = 1;
 }
 
 void
-resClass(struct jdump *jf, trbt_node_t *cnode)
+resolveClass(struct jdump *jf, trbt_node_t *cnode)
 {
     if (NULL == cnode)
         return;
-    resClass(jf, cnode->left);
-    resClassNode(jf, (cinfo *) cnode->data);
-    resClass(jf, cnode->right);
+    resolveClass(jf, cnode->left);
+    resolveClassNode(jf, (cinfo *) cnode->data);
+    resolveClass(jf, cnode->right);
 }
 
 void
-resolveClass(struct jdump *jf)
+resolveClasses(struct jdump *jf)
 {
     jf->javaLangClass = findClass(jf, "java/lang/Class");
-    jf->javaLangString = findClass(jf, "java/lang/String");
     jf->javaLangClassLoader = findClass(jf, "java/lang/ClassLoader");
 
-    resClass(jf, jf->cTable->root->left);
-    resClassNode(jf, (cinfo *) jf->cTable->root->data);
-    resClass(jf, jf->cTable->root->right);
+    resolveClass(jf, jf->cTable->root);
+}
+
+void
+resolveInstance(struct jdump *jf, hobject *ho)
+{
+    cinfo *ci;
+    int i;
+
+    if (ho->resolved)
+        return;
+    ho->resolved = 1;
+
+    if (H_VARRAY == ho->htype) {
+        if (0 == ho->count)
+            return;
+        fseeko(jf->fin, ho->fpos, SEEK_SET);
+        switch (ho->classId) {
+        case 4: case 8: { // BOOLEAN, BYTE
+            ho->val.b = (char *) talloc_array(ho, char, ho->count);
+            fread(ho->val.b, sizeof(char), ho->count, jf->fin);
+            break;
+        }
+        case 5: case 9: {       // CHAR, SHORT
+            ho->val.c = (unsigned short *) talloc_array(ho, unsigned short, ho->count);
+            fread(ho->val.c, sizeof(unsigned short), ho->count, jf->fin);
+            break;
+        }
+        case 6: case 7:         // FLOAT, DOUBLE
+            break;
+        case 10: {              // INT
+            ho->val.i = (unsigned int *) talloc_array(ho, unsigned int, ho->count);
+            fread(ho->val.i, sizeof(unsigned int), ho->count, jf->fin);
+            break;
+        }
+        case 11: {              // LONG
+            ho->val.j = (unsigned long long *) talloc_array(ho, unsigned long long, ho->count);
+            fread(ho->val.j, sizeof(unsigned long long), ho->count, jf->fin);
+            break;
+        }
+        }
+        return;
+    } else if (H_OARRAY == ho->htype) {
+        if (0 == ho->count)
+            return;
+        fseeko(jf->fin, ho->fpos, SEEK_SET);
+        ho->val.ident = (long long *) talloc_array(ho, long long, ho->count);
+        for (i = 0; i < ho->count; i++) 
+            *(ho->val.ident + i) = readIdent(jf);
+        for (i = 0; i < ho->count; i++) {
+            hobject *dref = (hobject *) trbt_lookup32(jf->hTable, (long) *(ho->val.ident + i));
+            if (dref)
+                resolveInstance(jf, dref);
+        }
+        return;
+    }
+    
+    ci = (cinfo *) trbt_lookup32(jf->cTable, ho->classId);
+
+    for (i = 0; i < ci->tfields; i++) {
+        finfo *value = *(ci->values + i);
+        fseeko(jf->fin, ho->fpos, SEEK_SET);
+        fseek(jf->fin, value->offset, SEEK_CUR);
+        switch (value->ftype) {
+        case '[': 
+        case 'L': {
+            value->val.ident = readIdent(jf);
+            hobject *dref = (hobject *) trbt_lookup32(jf->hTable, (long) value->val.ident);
+            if (0 && !dref) 
+                fprintf(stderr, "resolveInstance: cannot resolve Instance 0x%08x in 0x%08x of 0x%08x %s\n",
+                    value->val.ident, ho->instId, ho->classId, ci->name);
+            if (dref)
+                resolveInstance(jf, dref);
+            break;
+        }
+        case 'Z' : case 'B':
+            value->val.b = getc(jf->fin); break;
+        case 'S' :
+        case 'C' : readUshort(jf->fin, &(value->val.c)); break;
+        case 'I' : readUint(jf->fin, &(value->val.i)); break;
+        case 'J' : readUlong(jf->fin, &(value->val.j)); break;
+        }
+    }
+}
+
+void
+printInstance(struct jdump *jf, hobject *ho, int indent)
+{
+    int i;
+    cinfo *ci;
+
+    if (ho->visit) {
+        printf("[ recursive ] Instance 0x%08x of 0x%08x\n", ho->instId, ho->classId);
+        return;
+    }
+    if (H_VARRAY == ho->htype) {
+        printf("value array 0x%08x 0x%08x count %d\n", ho->instId, ho->classId, ho->count);
+        for (i = 0; i < ho->count; i++) {
+            switch (ho->classId) {
+            case 4: case 8:   printf("%c %x ", *(ho->val.b + i)); break;
+            case 5: case 9:   printf("%d ", *(ho->val.c + i)); break;
+            case 10:   printf("%d ", *(ho->val.i + i)); break;
+            case 11:   printf("%ld ", *(ho->val.j + i)); break;
+            }
+        }
+        return;
+    } else if (H_OARRAY == ho->htype) {
+        printf("object array 0x%08x 0x%08x count %d\n", ho->instId, ho->classId, ho->count);
+        for (i = 0; i < ho->count; i++) {
+            if (5 < i) {
+                puts("[....]");
+                break;
+            }
+            hobject *dref = (hobject *) trbt_lookup32(jf->hTable, (long) *(ho->val.ident + i));
+            if (dref)
+                printInstance(jf, dref, 0);
+            else
+                printf("Instance 0x%08x of 0x%08x %s\n", *(ho->val.ident + i), 0, "unknown");
+        }
+        return;
+    }
+
+    ci = (cinfo *) trbt_lookup32(jf->cTable, ho->classId);
+    ho->visit = 1;
+    printf("Instance 0x%08x of 0x%08x %s\n", ho->instId, ho->classId, ci->name);
+    if (indent)
+        printf("---- (%d)\n", indent);
+    for (i = 0; i < ci->tfields; i++) {
+        finfo *value = *(ci->values + i);
+        
+        if (indent) fputs("\t", stdout);
+        printf("\t%3d (%3d): %c %-25s ", i, value->offset, value->ftype, value->name);
+        switch (value->ftype) {
+        case '[' :
+        case 'L' : {
+            hobject *dref = (hobject *) trbt_lookup32(jf->hTable, (long) value->val.ident);
+            if (dref)
+                printInstance(jf, dref, 1 + indent);
+            else
+                printf("Instance 0x%08x of 0x%08x %s\n", value->val.ident, 0, "unknown");
+            break;
+        }
+        case 'B' :
+        case 'Z' :  printf(" %c  0x%x\n", value->val.b, value->val.b);  break;
+        case 'C' :
+        case 'S' :  printf(" %d  0x%x\n", value->val.c, value->val.c);  break;
+        case 'I' :  printf(" %d  0x%x\n", value->val.i, value->val.i);  break;
+        case 'J' :  printf(" %ld  0x%lx\n", value->val.j, value->val.j);  break;
+        default: puts(" 0 0 ");
+        }
+    }
+    if (indent)
+        printf("---- (%d)\n", indent);
+    ho->visit = 0;
+}
+
+void
+collectStats(struct jdump *jf, trbt_node_t *node, unsigned long ckey)
+{
+    hobject *ho;
+    if (NULL == node)
+        return;
+    collectStats(jf, node->left, ckey);
+    ho = (hobject *) node->data;
+    if (ckey == ho->classId) {
+        resolveInstance(jf, ho);
+        printInstance(jf, ho, 0);
+    }
+    collectStats(jf, node->right, ckey);
+}
+
+unsigned int 
+countbytes(FILE *fin, unsigned int sz)
+{
+    unsigned int count = 0;
+    int bb;
+
+    while (sz > 0 && EOF != (bb = getc(fin))) {
+        if (0 < bb) 
+            count++;
+        sz--;
+    }
+    return count;
 }
 
 void
@@ -684,6 +935,8 @@ readHeap(struct jdump *jf, unsigned int hsize)
 
 
     roott = rootg = rootl = frame = stack = sclass = tblock = monitor = cclass = inst = oarr = parr = 0;
+
+    jf->javaLangString = findClass(jf, "java/lang/String");
 
     while (0 < hsize && EOF != (rtype = getc(jf->fin))) {
         hsize--;
@@ -799,8 +1052,8 @@ readHeap(struct jdump *jf, unsigned int hsize)
         }
         case 0x21 : {    // HPROF_GC_INSTANCE_DUMP
             long long ide, classId;
-            unsigned int stackId, isz;
-            struct hobj *ho;
+            unsigned int stackId, isz, cnt, fpos;
+            hobject *ho;
             cinfo *ci;
 
             ide = readIdent(jf);
@@ -812,8 +1065,13 @@ readHeap(struct jdump *jf, unsigned int hsize)
             ci->count++;
             if (debug)
                 printf("0x%x instance 0x%x %s\n", ide, classId, ci->name);
-            // ho = makeObj(jf->hTable, H_INSTANCE, classId);
-            // trbt_insert32(jf->hTable, ide, ho);
+            // fpos = ftell(jf->fin);
+            // cnt = countbytes(jf->fin, isz);
+            if (50000 > ci->count || classId == jf->javaLangString->ident) {
+                ho = makeObj(jf->hTable, H_INSTANCE, ide, classId);
+                ho->fpos = ftello(jf->fin);
+                trbt_insert32(jf->hTable, ide, ho);
+            }
             // putchar('i');
             fseek(jf->fin, isz, SEEK_CUR);
             inst++;
@@ -854,8 +1112,13 @@ readHeap(struct jdump *jf, unsigned int hsize)
     printf("\t%15s : %8d \n", "object array", oarr);
     printf("\t%15s : %8d \n", "primary array", parr);
 
-    resolveClass(jf);
+    resolveClasses(jf);
 
     puts("Class Summary");
-    printclass(jf->cTable);
+    printClass(jf->cTable->root);
+
+    {
+        cinfo *cdata = findClass(jf, "com/teramedica/web/actions/notification/TMNotificationListAction");
+        collectStats(jf, jf->hTable->root, (long) cdata->ident);
+    }
 }
